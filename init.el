16fad8f66232f3f9b2261fd37c180b31d2162618
Samuel Merritt <sam@swiftstack.com>
2014-03-31 13:51:28 -0700

    customize

--- init.el
+++ init.el
@@ -206,3 +206,15 @@
 ;; not until it's stable
 ;;(load "~/.emacs.d/floobits/floobits.el")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
