80fec882ed614b0c7919e068edf5757ced733f38
Samuel Merritt <sam@swiftstack.com>
2018-05-11 12:41:35 -0700

    Get rid of insecure package repo

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 ;; install extra packages
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
