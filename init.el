9c0b20b3f91dc963b11c160c2c0e08a708803a7f
Samuel Merritt <spam@andcheese.org>
2023-03-18 03:42:27 +0000

    okay, actually commit init.el

--- init.el
+++ init.el
@@ -1,37 +1,37 @@
-;; install extra packages
+;; MELPA for packages. The GNU repos are enabled by default.
+
 (require 'package)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; SSH on Windows requires plink
+(require 'tramp)
+(if (eq system-type 'windows-nt)
+    (setq tramp-default-method "plink"))
 
-(defvar my-packages
-  '(go-mode
-    paredit
-    clojure-mode
-    markdown-mode
-    twilight-anti-bright-theme
-    smex
-    yasnippet
-    unicode-fonts
-		find-file-in-project)
-  "A list of packages to ensure are installed at launch.")
+;; Solarized is a nice theme, but the grayish background on an LCD
+;; monitor is off-putting.
+(require 'solarized-theme)
+(load-theme 'solarized-dark t)
+(set-background-color "black")
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
 
-(require 'twilight-anti-bright-theme)
-(require 'yasnippet)
+;; iswitchb is dead; long live ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+(setq
+ ;; Don't try to connect to something just because its name is under
+ ;; point. It just locks up Emacs for a while and pisses me off.
+ ido-use-filename-at-point nil
 
-;;;; Basic setup: colors, modifier keys, builtin behaviors, etc.
-;; Emacs for OSX has this toolbar with stuff like save, open,
+ ido-enable-flex-matching t
+ )
+
+;; GUI Emacs has this toolbar with stuff like save, open,
 ;; copy, paste... I'd rather have 2 more lines of text.
 (tool-bar-mode 0)
+
+;; Miscellaneous
 
 (setq
  kill-whole-line t
@@ -42,30 +42,21 @@
  split-width-threshold 200
  )
 
-(delete-selection-mode)
-(random t)                              ; reseed
-(server-start)
-(global-auto-revert-mode 1)
 (global-linum-mode 1)
-(unicode-fonts-setup)
+
+;; Don't minimized on Ctrl-z (C-x C-z is fine, though)
+(global-unset-key "\C-z")
+
+;; Let me type over things
+(delete-selection-mode)
+
+;; Switch between windows with shift-arrow
 (windmove-default-keybindings)
-
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-(global-set-key [(f6)] 'next-error)
-(global-set-key [(shift f6)] 'previous-error)
-(global-set-key [(control \;) ?f ?f] 'ffip)
-(global-set-key [(control tab)] 'hippie-expand)
-(global-set-key [(shift tab)] 'hippie-expand)
-(global-set-key [(backtab)] 'hippie-expand)
-(global-set-key [(control c) ?r] 'revert-buffer)
 
 ;; I never really want zap-to-char. I pretty much always want the
 ;; character that I'm zapping to.
 (global-set-key [(meta z)] 'zap-up-to-char)
+
 
 ;; C-' to start/end macro, C-M-' to run it
 (defun start-or-end-kmacro ()
@@ -73,49 +64,15 @@
   (if defining-kbd-macro
       (kmacro-end-macro nil)
     (kmacro-start-macro nil)))
-
 (global-set-key [(control \')] 'start-or-end-kmacro)
 (global-set-key [(control meta \')] 'kmacro-end-and-call-macro)
 
-;; on kinesis freestyle, 'Delete' sends kp-delete
-(global-set-key [kp-delete] 'delete-char)
-(global-set-key [(control kp-delete)] 'kill-word)
 
 ;; Mac-isms. They do no harm on non-macs.
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 
-;; don't iconify on C-z when running in X
-;; or exit emacs (!) when running in Emacs.app
-(when window-system (global-unset-key "\C-z"))
-
-;; make yasnippet and hippie-expand play well together
-(setq hippie-expand-try-functions-list
-            '(yas/hippie-try-expand
-                try-expand-dabbrev
-                try-expand-dabbrev-all-buffers
-                try-expand-dabbrev-from-kill))
-
-;; make snippets available in programming modes (I'm not sure why
-;; yas/hippie-try-expand needs this, but it never finds snippets
-;; unless you do this)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
-
-;; just nice to have when writing code
-(defun my-prog-mode-hook ()
-    (column-number-mode)
-    (setq tab-width 4)
-    (show-paren-mode 1))
-
-(add-hook 'prog-mode-hook 'my-prog-mode-hook)
-
-
-;; visible bell on El Capitan is crap
-(setq visible-bell nil)
-(setq ring-bell-function
-      (lambda ()
-        (invert-face 'mode-line)
-        (run-with-timer 0.1 nil 'invert-face 'mode-line)))
+;; Utility functions for generating random data
 
 
 (defun random-ipv4-address ()
@@ -184,7 +141,6 @@
                                     (random (expt 16 4))
                                     (random (expt 16 12)))))
 
-
 (global-set-key [(control \;) ?r ?i ?4] 'insert-random-ipv4-address)
 (global-set-key [(control \;) ?r ?i ?6] 'insert-random-ipv6-address)
 (global-set-key [(control \;) ?r ?h] 'insert-random-hash)
@@ -196,22 +152,29 @@
 (global-set-key [(control \;) ?n ?t] 'insert-nano-time)
 
 
-(add-hook
- 'html-mode-hook
- (lambda ()
-   (set (make-local-variable 'sgml-basic-offset) 2)))
+;; Take all the windows in the current frame and shift them over one.
+;;
+;; With 2 windows, effectively switches their positions.
+;;
+;; With 1 window, this is a no-op.
+;;
+;; With 3 or more windows, this confuses the user.
+(defun rotate-windows ()
+  (interactive)
+  (let ((buffers (mapcar 'window-buffer (window-list))))
+    (mapcar* 'set-window-buffer
+             (window-list)
+             (append (cdr buffers) (list (car buffers))))))
 
-;;;; python stuff
+(global-set-key [(control \;) ?r ?w] 'rotate-windows)
+
+;;
+;; Python stuff
+;;
 (defun python-insert-pdb-breakpoint ()
   (interactive)
   (indent-for-tab-command)
   (insert "import pdb; pdb.set_trace()  ### XXXXXXXXXXXXXXXXXXXXXXXX\n")
-  (indent-for-tab-command))
-
-(defun python-insert-dprint ()
-  (interactive)
-  (indent-for-tab-command)
-  (insert "from common.pp import dprint\n")
   (indent-for-tab-command))
 
 (defun python-insert-q-q ()
@@ -227,6 +190,16 @@
   (backward-char))
 
 (defun python-auto-constructor ()
+  "
+    Turns
+      def __init__(self, a, b, c):
+
+    into
+      def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+  "
     (interactive)
     (search-backward "self, ")
     (forward-char 6)
@@ -258,53 +231,13 @@
     (local-set-key [(control \;) ?a ?c] 'python-auto-constructor)
     (local-set-key [(control \;) ?b ?p] 'python-insert-pdb-breakpoint)
     (local-set-key [(control \;) ?q ?q] 'python-insert-q-q)
-    (local-set-key [(control \;) ?p ?p] 'python-insert-pprint)
-    (local-set-key [(control \;) ?d ?p] 'python-insert-dprint))
+    (local-set-key [(control \;) ?p ?p] 'python-insert-pprint))
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
-;;;; Protobuf mode
-(defconst my-protobuf-style
-  ;; 2-space indentation, no tabs
-  '((c-basic-offset . 2)
-    (indent-tabs-mode . nil)))
-
-(add-hook 'protobuf-mode-hook
-          (lambda ()
-            (c-add-style "my-style" my-protobuf-style t)))
-
-
-;;;; Elisp stuff
-(defun my-emacs-lisp-mode-hook ()
-    (paredit-mode)
-    (setq tab-width 2))
-
-(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
-
-;; Take all the windows in the current frame and shift them over one.
 ;;
-;; With 2 windows, effectively switches their positions.
+;; ffip configuration
 ;;
-;; With 1 window, this is a no-op.
-;;
-;; With 3 or more windows, this confuses the user.
-(defun rotate-windows ()
-  (interactive)
-  (let ((buffers (mapcar 'window-buffer (window-list))))
-    (mapcar* 'set-window-buffer
-             (window-list)
-             (append (cdr buffers) (list (car buffers))))))
-
-(global-set-key [(control \;) ?r ?w] 'rotate-windows)
-
-;; Kill region if active (for Darrell)
-(defun kill-region-or-line ()
-  (interactive)
-  (if (use-region-p)
-      (kill-region (region-beginning) (region-end))
-    (kill-line)))
-
-(global-set-key [(control k)] 'kill-region-or-line)
 
 ;; Find everything, dammit (except stuff in .git)
 (require 'find-file-in-project)
@@ -329,94 +262,10 @@
 (add-to-list 'ffip-prune-patterns "*/build/*") ;; Python: leftovers from "setup.py install"
 (add-to-list 'ffip-prune-patterns "*/.eggs/*") ;; Python: more leftovers from "setup.py install"
 
+;; Don't even generate filenames in .tox only to ignore them later; it's slow
 (setq ffip-find-options (concat ffip-find-options " -not -regex \".*/.tox/.*\""))
 
-;;;; ido-mode is basically magic
-(ido-mode t)
 
-(setq
- ;; Don't try to connect to something just because its name is under
- ;; point. It just locks up Emacs for a while and pisses me off.
- ido-use-filename-at-point nil
-
- ido-enable-flex-matching t
- )
-
-(setq lua-indent-level 2)  ;; default is 3; who uses 3 spaces?
-
-(require 'grep)
-(add-to-list 'grep-find-ignored-directories ".venv")
-(add-to-list 'grep-find-ignored-directories ".tox")
-(add-to-list 'grep-find-ignored-directories "migrations")
-(add-to-list 'grep-find-ignored-directories "build")
-
-
-;; the default C indentation is pretty horrible
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (setq fill-column 78)
-            (c-set-style "linux")
-            (setq c-basic-offset 4)))
-
-;;;; golang stuff
-;; If you install your own Go environment, it typically ends up in
-;; /usr/local/bin.
-(setq exec-path (cons "/usr/local/bin" exec-path))
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-
-;; goimports uses this to find my imports
-(setenv "GOPATH" (concat (getenv "HOME") "/go"))
-
-(defun go-if-err (preerr posterr)
-  (let ((action (format "%serr%s" preerr posterr)))
-    (dolist (line `("if err != nil {"
-                    ,action
-                    "}"))
-      (insert line)
-      (indent-for-tab-command)
-      (newline))
-    (delete-backward-char 1))) ; one too many newlines
-
-(defun go-return-if-err ()
-  (interactive)
-  (go-if-err "return " ""))
-
-(defun go-panic-if-err ()
-  (interactive)
-  (go-if-err "panic(" ")"))
-
-(defun go-assert-return-if-err ()
-  (interactive)
-    (dolist (line `("if !assert.Nil(err) {"
-                                    "return"
-                                    "}"))
-        (insert line)
-        (indent-for-tab-command)
-        (newline))
-    (delete-backward-char 1)) ; one too many newlines
-
-(defun my-go-mode-hook ()
-  (setq tab-width 4)
-    (setq fill-column 120)
-  (setq gofmt-command "~/go/bin/goimports")
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (local-set-key (kbd "M-=") (lambda () (interactive) (insert ":=")))
-    (local-set-key [(control \;) ?r ?e] 'go-return-if-err)
-    (local-set-key [(control \;) ?p ?e] 'go-panic-if-err)
-    (local-set-key [(control \;) ?a ?e] 'go-assert-return-if-err))
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
-;; actually load snippets
-(yas-reload-all)
-
-
-;; if there's some machine-specific configuration, go ahead and load it
-(let* ((dotfiles-dir (file-name-directory
-											(or (buffer-file-name) load-file-name)))
-			 (machine-specific-config-file (concat dotfiles-dir (system-name) ".el")))
-	(if (file-exists-p machine-specific-config-file)
-			(load machine-specific-config-file)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -424,21 +273,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-     (quote
-        (yasnippet gorepl-mode god-mode unicode-fonts twilight-theme smex paredit markdown-mode go-mode find-file-in-project color-theme clojure-mode)))
- '(safe-local-variable-values
-     (quote
-        ((eval c-set-offset
-                     (quote innamespace)
-                     0)
-         (encoding . utf-8)
-         (whitespace-line-column . 80)
-         (ido-use-virtual-buffers . t)
-         (lexical-binding . t)))))
+   '(find-file-in-project eglot pytest pytest-pdb-break python-black solarized-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'downcase-region 'disabled nil)
